3575e4ca8d7d87c3027e5d6bdb99e9881564840e
EriozDev <172150819+EriozDev@users.noreply.github.com>
2025-09-10 20:13:55 +0200

    oeoe

--- client/libs/ped.lua
+++ client/libs/ped.lua
@@ -21,6 +21,7 @@ function _ped.new()
     self.name = GetPlayerName(playerId)
     self.ped = nil
     self.ch = 0
+    self.IsFreeze = false
 
     setmetatable(self, __instance)
     p_Index[serverId] = self
@@ -113,5 +114,86 @@ function _ped:spawn(coords, heading)
         Resources[i]:start()
     end
 end
+
+function _ped:setClothes(componentId, drawableId, textureId, paletteId)
+    if not self.ped or not DoesEntityExist(self.ped) then
+        return false
+    end
+    SetPedComponentVariation(self.ped, componentId, drawableId, textureId, paletteId or 0)
+    return true
+end
+
+function _ped:getClothes(componentId)
+    if not self.ped or not DoesEntityExist(self.ped) then
+        return nil
+    end
+    local drawable = GetPedDrawableVariation(self.ped, componentId)
+    local texture = GetPedTextureVariation(self.ped, componentId)
+    local palette = GetPedPaletteVariation(self.ped, componentId)
+    return {drawable = drawable, texture = texture, palette = palette}
+end
+
+function _ped:saveOutfit()
+    if not self.ped or not DoesEntityExist(self.ped) then
+        return false
+    end
+    local outfit = {}
+    for i = 0, 11 do
+        outfit[i] = self:getClothes(i)
+    end
+    self.__var["outfit"] = outfit
+    return outfit
+end
+
+function _ped:loadOutfit(outfit)
+    outfit = outfit or self.__var["outfit"]
+    if not outfit then return false end
+    for compId, data in pairs(outfit) do
+        if data then
+            self:setClothes(compId, data.drawable, data.texture, data.palette)
+        end
+    end
+    return true
+end
+
+function _ped:applyOutfit(outfit)
+    if not outfit then return false end
+    for compId, data in pairs(outfit) do
+        if data then
+            self:setClothes(compId, data.drawable or 0, data.texture or 0, data.palette or 0)
+        end
+    end
+    return true
+end
+
+function _ped:setFreezeState(v)
+    if type(v) ~= 'boolean' then
+        return false;
+    end
+    self.IsFreeze = v
+    FreezeEntityPosition(self.ped, self.IsFreeze)
+end
+
+function _ped:setVisible(state, selfVisible)
+    if not self.ped or not DoesEntityExist(self.ped) then
+        return false
+    end
+
+    if state then
+        SetEntityVisible(self.ped, true, false)
+        SetLocalPlayerVisibleLocally(self.ped, true)
+    else
+        SetEntityVisible(self.ped, false, false)
+
+        if selfVisible then
+            SetLocalPlayerVisibleLocally(self.ped, true)
+        else
+            SetLocalPlayerVisibleLocally(self.ped, false)
+        end
+    end
+
+    return true
+end
+
 
 ERZ.lib['_ped'] = _ped
